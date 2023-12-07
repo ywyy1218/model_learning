483cea06e0eaf8048b7bf55b4d001841af07853f
ywyy1218 <ywyy1218@163.com>
2023-12-07 20:36:19 +0800

    model

---
+++ LSTM/LSTM2/model.py
@@ -0,0 +1,12 @@
+# wjx
+# 2023/11/22 20:45
+import torch.nn as nn
+class AirModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=1, hidden_size=50, num_layers=1, batch_first=True)
+        self.linear = nn.Linear(50, 1)
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.linear(x)
+        return x
